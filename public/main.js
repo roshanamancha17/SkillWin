a15dc7c273a076f63001b0893d3974c68864e34b
roshanamancha <roshan.m.amancha17@gmail.com>
2025-04-20 04:44:54 +0530

    authentication 3

--- public/main.js
+++ public/main.js
@@ -43,10 +43,10 @@ if (playButton) {
 
 // Your Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyBmONfeIStjrxO1lSnLKCOIveLPN-udJbs",
+  apiKey: "AIzaSyBmONfelStjrxOl1SnLKCOIveLPN-udJbs",
   authDomain: "skill-win-d8c81.firebaseapp.com",
   projectId: "skill-win-d8c81",
-  storageBucket: "skill-win-d8c81.appspot.com",
+  storageBucket: "skill-win-d8c81.firebasestorage.app",
   messagingSenderId: "808700132713",
   appId: "1:808700132713:web:1ab8376f139278f89ad1f8",
   measurementId: "G-V9S0F1XD20"
