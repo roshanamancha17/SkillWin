1003ea0e820d3fdaeb1e9208ff3722311cea5028
roshanamancha <roshan.m.amancha17@gmail.com>
2025-04-28 08:59:39 +0530

    test

--- public/games-Info/html/javascript/coin.js
+++ public/games-Info/html/javascript/coin.js
@@ -1,14 +1,5 @@
 // Initialize Firebase
-const firebaseConfig = {
-  // your config here
-  apiKey: "AIzaSyBmONfelStjrxOl1SnLKCOIveLPN-udJbs",
-  authDomain: "skill-win-d8c81.firebaseapp.com",
-  projectId: "skill-win-d8c81",
-  storageBucket: "skill-win-d8c81.firebasestorage.app",
-  messagingSenderId: "808700132713",
-  appId: "1:808700132713:web:1ab8376f139278f89ad1f8"
-};
-firebase.initializeApp(firebaseConfig);
+
 const db = firebase.firestore();
 const auth = firebase.auth();
 
